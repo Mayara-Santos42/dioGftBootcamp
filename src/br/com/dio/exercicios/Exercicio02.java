3b06d0c9881646fe87b538d892a02fd2846f6ec8
Mayara-Santos42 <may.santoss1@outlook.com>
2022-05-02 22:46:57 -0300

    exercicios

--- src/br/com/dio/exercicios/Exercicio02.java
+++ src/br/com/dio/exercicios/Exercicio02.java
@@ -5,23 +5,32 @@ import java.util.Calendar;
 public class Exercicio02 {
 
 	public static void main(String[] args) {
-		Calendar horas = Calendar.getInstance();
-		int horarioAtual = horas.get(Calendar.HOUR);
 		
-		int mensagem = mensagem();
-	
-		System.out.println(horarioAtual);
+		Calendar horas = Calendar.getInstance();
+		int horarioAtual = horas.get(Calendar.HOUR_OF_DAY);
+		
+		int getMensagem = getMensagem(horarioAtual);
+
+		System.out.println("São "  + getMensagem + " Horas");
+		
+		
 	}
 	
-	public static int mensagem() {
+	public static int getMensagem(int horarioAtual) {
 		
-		int horarioAtual = 0;
-		if(horarioAtual > 0 && horarioAtual <= 6) {
-			System.out.println("Boa Madrugada!");
+		if(horarioAtual >= 6 && horarioAtual < 12){
+			System.out.println("Bom dia!");
+		} else if (horarioAtual >= 12 && horarioAtual < 18){
+			System.out.println("Boa tarde!");			
+		} else {
+			System.out.println("Boa noite");
 		}
-		return mensagem();
+		return horarioAtual;		
 	}
 }
+
+
+
 
 
 	
